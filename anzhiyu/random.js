afa907a9a75ea447a6179e06e23518b1e0ad5afe
SeagullOO <3164220387@qq.com>
2026-03-12 00:22:04 +0800

    Site updated: 2026-03-12 00:22:02

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2025/09/23/Seagull-Game-暂定-开发日记/","2025/09/12/Hexo+Github搭建个人网站笔记/","2025/09/13/安知鱼主题自定义教程/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
